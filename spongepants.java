4ca7511d5152f7bbb4edbe7504eb661cafdf8d2f
davidPies <2014119@edtools.psd401.net>
2016-06-09 11:18:04 -0700

    Update spongepants.java

--- spongepants.java
+++ spongepants.java
@@ -5,11 +5,12 @@ package spongebobmod;
 //import [...];
 
 public class ItemModArmor extends ItemArmor {
-
+  public static ArmorMaterial pants = EnumHelper.addArmorMaterial("pants", "spongebobmod:spongepants", 10, new int[] {0, 0, 5, 0}, );
+  public static ArmorMaterial dome = EnumHelper.addArmorMaterial("dome", "spongebobmod:dome", 10, new int[] {7, 0, 0, 0}, );
     public ItemModArmor(String unlocalizedName, ArmorMaterial material, int renderIndex, int armorType) {
         super(material, renderIndex, armorType);
         GameRegistry.registerItem(spongepants = new spongepants("spongepants", pants, 2, 2), "spongepants");
-
+        GameRegistry.registerItem(spongepants = new spongepants("spongepants", pants, 2, 2), "spongepants");
         this.setUnlocalizedName("spongepants");
     }
     //krusty krab pizza rock entity and pizza box with pizza and model for both
